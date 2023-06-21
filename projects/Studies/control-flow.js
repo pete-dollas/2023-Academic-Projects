425ac48938e55588754c5e202717f22ce44cf58e
Pete Dollas <pdollas@gmail.com>
2023-06-21 18:19:15 +0000

    studies main topic populated in each page

--- projects/Studies/control-flow.js
+++ projects/Studies/control-flow.js
@@ -2,4 +2,12 @@
  * CONTROL FLOW
  * 
  * 0. DEFINITION & BACKGROUND
+ * 
+ * 1. IF
+ * 
+ * 2. ELSE-IF
+ * 
+ * 3. ELSE
+ * 
+ * 4. SWITCH
  */
--- projects/Studies/datatypes.js
+++ projects/Studies/datatypes.js
@@ -2,4 +2,30 @@
  * DATA TYPES
  * 
  * 0. DEFINITION & BACKGROUND
+ * 
+ * 1. NUMBER
+ * 
+ * 2. STRING
+ * 
+ * 3. BOOLEAN
+ * 
+ * 4. ARRAY
+ * 
+ * 5. OBJECT
+ * 
+ * 6. FUNCTION
+ * 
+ * 7. UNDEFINED
+ * 
+ * 8. NULL
+ * 
+ * 9. NaN
+ * 
+ * 10. INFINITY & -INFINITY
+ * 
+ * 11. PRIMITIVE/SIMPLE VS COMPLEX
+ * 
+ * 12. Primitives passed to a function BY COPY and complex values are passed BY REFERENCE.
+ * COMPARE:
+ * CONTRAST:
  */
