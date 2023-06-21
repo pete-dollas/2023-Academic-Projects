5cb1432cb09cf543f36fa69a67dbfffbc0e05c8f
Pete Dollas <pdollas@gmail.com>
2023-06-21 17:53:12 +0000

    updated operators and variables

---
+++ projects/Studies/operators.js
@@ -0,0 +1,34 @@
+/**
+ * OPERATORS
+ */
+
+//1. ASSIGNMENT OPERATORS
+//every single one
+
+//2. ARITHMETIC OPERATORS
+//every single one
+Operator	Description	Note
++	Addition	 
+-	Subtraction	 
+*	Multiplication	 
+**	Exponent - normal	2**3 = 8 (2 cubed)
+e	Exponent - scientific notation where e is 10 followed by exponent	2e3 = 2000 (2 * 10 to the 3rd)
+/	Division	 
+%	Modulus (What the remainder is)	9%2 = 1 (9 divided by 2 is 8R1)
+++	Increment	up by 1
+--	Decrement	down by 1
+ +=	Add this much	 
+ -=	Subtract this much	x+=3 is add 3 to x, or 4+=5 is 9
+*=	Multiply by this much	 
+/=	Divide by this much	 
+
+//3. COMPARISON OPERATORS
+//every single one
+
+//4. LOGICAL OPERATORS
+//every single one
+
+//5. UNARY OPERATORS (!, typeOf, -)
+//don't need to show every single one
+
+//6. TERNARY OPERATORS (a ? b : c)
---
+++ projects/Studies/variables.js
@@ -0,0 +1,54 @@
+/**
+ * VARIABLES
+ * 
+ * 0: Variables are named containers
+ * 
+ * 1: Declaration and assignment:
+ * 
+ * 2: var, let, const
+ * 
+ * 3: Hoisting
+ */
+
+
+//1. Declaration and assignment
+
+//2 var
+
+//2 let
+
+//const
+
+//3 Hoisting
+//var vs const vs let
+/** 
+console.log(hello);
+var hello = "Hi"; 
+//undefined
+*/
+
+/** 
+console.log(hello);
+let hello = "Hi";
+//ReferenceError: Cannot access 'hello' before initialization
+*/
+
+/** 
+console.log(hello);
+const hello = "Hi";
+//ReferenceError: Cannot access 'hello' before initialization
+*/
+
+
+/** 
+var hello = "Hi"; 
+console.log(hello);
+//Hi
+*/
+
+
+//eg function followed by...
+//const = error
+
+//const
+//function == works
