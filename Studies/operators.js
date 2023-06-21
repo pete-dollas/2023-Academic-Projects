96dfd440edf1ba9cb2ad2640beea7a455bf2baef
Pete Dollas <pdollas@gmail.com>
2023-06-21 06:36:26 +0000

    Studies skeleton files set up

---
+++ Studies/operators.js
@@ -0,0 +1,20 @@
+/**
+ * OPERATORS
+ */
+
+//1. ASSIGNMENT OPERATORS
+//every single one
+
+//2. ARITHMETIC OPERATORS
+//every single one
+
+//3. COMPARISON OPERATORS
+//every single one
+
+//4. LOGICAL OPERATORS
+//every single one
+
+//5. UNARY OPERATORS (!, typeOf, -)
+//don't need to show every single one
+
+//6. TERNARY OPERATORS (a ? b : c)
--- Studies/variables.js
+++ Studies/variables.js
@@ -9,3 +9,21 @@
  * 
  * 3: Hoisting
  */
+
+
+//1. Declaration and assignment
+
+//2 var
+
+//2 let
+
+//const
+
+//3 Hoisting
+//var vs const vs let
+
+//eg function followed by...
+//const = error
+
+//const
+//function == works
