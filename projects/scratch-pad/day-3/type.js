49c1c95c95af62bd4275838f13751444fe8aa313
Pete Dollas <pdollas@gmail.com>
2023-06-21 04:54:11 +0000

    Day 3 Range & Type completed; first class functions begun

--- projects/scratch-pad/day-3/type.js
+++ projects/scratch-pad/day-3/type.js
@@ -12,9 +12,11 @@
  * TIP: In JavaScript, how can we decipher if a value is an Array? Can typeof
  * work?
  */
-function isArray(value) {
+function isArray(value) {//input is any value
     // YOUR CODE BELOW HERE //
-    
+ return Array.isArray(value)////return boolean based on isArray results on input
+ 
+
     
     
     
@@ -29,12 +31,14 @@ function isArray(value) {
  * null, not an Array, not a Date - all of these will return 'object' if used 
  * with typeof.
  */
-function isObject(value) {
+function isObject(value) {//input any value
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    if (value instanceof Object === true//is an object
+        && value !== null//is not null
+        && value instanceof Array === false//is not an array
+        && value instanceof Date === false)//is not a Date
+        {return true}
+        else {return false}
     // YOUR CODE ABOVE HERE //
 }
 
@@ -44,12 +48,19 @@ function isObject(value) {
  * 
  * TIP: Similar to isObject, but we must return true if the value is an Array.
  */
-function isCollection(value) {
+function isCollection(value) {//input a value
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    if (value instanceof Object === true//is an object
+        && value instanceof Array === true//and is an array
+        && value !== null//cannot be null
+        && value instanceof Date === false)//cannot be a Date
+        {return true}
+        else if (value instanceof Object === true//is an object
+        && value instanceof Array === false//and is NOT an array
+        && value !== null//cannot be null
+        && value instanceof Date === false)//cannot be a Date
+        {return true}
+        else {return false}
     // YOUR CODE ABOVE HERE //
 }
 
@@ -72,12 +83,16 @@ function isCollection(value) {
  *    typeOf("javascript") -> "string"
  *    typeOf([1,2,3]) -> "array"
  */ 
-function typeOf(value) {
+function typeOf(value) {//input any value
     // YOUR CODE BELOW HERE //
-    
-    
-    
-    
+    if (value === null) {return "null"} //log as string
+    else if (value instanceof Object === true) {////if value is an object, use else if to determine what kind
+        if (value instanceof Array === true) {return "array"}//log as string
+        else if (value instanceof Date === true) {return "date"}//log as a string
+        else if (value instanceof Function === true) {return "function"}
+        else {return "object"}
+    }
+    else {return typeof(value)}
     // YOUR CODE ABOVE HERE //
 }
 
