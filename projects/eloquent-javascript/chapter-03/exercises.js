1957394d7f0af166f122476473693b4438860091
Pete Dollas <pdollas@gmail.com>
2023-06-30 02:54:30 +0000

    added instructions for each Eloquent Javascript problem in Ch3

--- projects/eloquent-javascript/chapter-03/exercises.js
+++ projects/eloquent-javascript/chapter-03/exercises.js
@@ -1,6 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // min /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//“Write a function min that takes two arguments and returns their minimum.”
 
 function min() {
 
@@ -9,6 +10,15 @@ function min() {
 ////////////////////////////////////////////////////////////////////////////////
 // isEven //////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//RECURSION: “Here’s another way to define whether a positive whole number is even or odd:
+/*
+Zero is even.
+One is odd.
+For any other number N, its evenness is the same as N –2.
+
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+Test it on 50 and 75. See how it behaves on –1. Why? Can you think of a way to fix this?”
+*/
 
 function isEven() {
 
@@ -17,6 +27,11 @@ function isEven() {
 ////////////////////////////////////////////////////////////////////////////////
 // countChars //////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//“Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+//Next, write a function called countChar that behaves like countBs, 
+//except it takes a second argument that indicates the character that is to be counted 
+//(rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.”
+
 
 function countChars() {
 
