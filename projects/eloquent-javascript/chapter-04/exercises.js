fa4e697c8a97f5b9a8b8b1a7d87ada1dc25eebc2
Pete Dollas <pdollas@gmail.com>
2023-06-30 02:59:33 +0000

    Added instructions to all problems in Eloquent JS Ch4

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -1,11 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//THE SUM OF A RANGE PART 1 P.180
+//“Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+//Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+//As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
 function range() {
 
 }
-
+//THE SUM OF A RANGE PART 2 P.180 (SEE ABOVE)
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -14,9 +18,16 @@ function sum() {
 
 }
 
+
+//REVERSING AN ARRAY PARTS 1 & 2 P181
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArray ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//“For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+//The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order.
+// The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. 
+//Neither may use the standard reverse method.”
+
 
 function reverseArray() {
 
@@ -33,6 +44,11 @@ function reverseArrayInPlace() {
 ////////////////////////////////////////////////////////////////////////////////
 // arrayToList /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//“Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+//Also write a listToArray function that produces an array from a list. 
+//Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+//If you haven’t already, also write a recursive version of nth.”
+
 
 function arrayToList() {
 
@@ -65,6 +81,9 @@ function nth() {
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//“Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual.
+//To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+//The Object.keys function will be useful when you need to go over the properties of objects to compare them.”
 
 function deepEqual() {
 
