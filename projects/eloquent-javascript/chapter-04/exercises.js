7c330279b0f31de06e80b612c61811bb83e75b24
Pete Dollas <pdollas@gmail.com>
2023-06-30 23:00:36 +0000

    Completed Eloquent JSCh4 Range,Sum,SumOfRange

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -2,21 +2,65 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 //THE SUM OF A RANGE PART 1 P.180
-//“Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
-//Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
-//As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+//“Write a range function that takes two arguments, start and end, and 
+//returns an array containing all the numbers from start up to (and including) end.
 
-function range() {
 
+/*
+function range (start, end){
+  let newArr = [];
+  for (let i = start; i <= end; i++) {
+    newArr.push(i)
+  }
+  return newArr;
 }
+console.log(range(2,11));
+*/
+//“Write a range function that takes two arguments, start and end, and 
+//returns an array containing all the numbers from start up to (and including) end.
+//BONUS-(SOLVED) modify your range function to take an optional third argument that indicates the “step” value 
+//used when building the array. 
+//If no step is given, the elements go up by increments of one, corresponding to the old behavior. 
+//The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. 
+//Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].”
+
+function range (start, end, step){
+  let newArr = [];
+  if (start === end) {return []}
+  else if (!step) {
+    for (let i = start; i <= end; i++){ 
+    newArr.push(i)
+  }}
+  else if (step > 0) {
+    for (let i = start; i <= end; i+= step){
+    newArr.push(i)}
+  }
+  else if (step < 0) {
+    for (let i = start; i >= end; i+= step){
+    newArr.push(i)}
+  }
+  return newArr;
+}
+
+
 //THE SUM OF A RANGE PART 2 P.180 (SEE ABOVE)
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function sum() {
+//PART2-(SOLVED) “Next, write a sum function that takes an array of numbers and returns the sum of these numbers. 
+//Run the example program and see whether it does indeed return 55.”
 
+
+function sum(arr){
+  let total = 0;
+  for (let i = 0; i <= arr.length - 1; i++) {
+    total +=arr[i];
+  }
+  return total;
 }
+let array1 = [1, 2, 3, 6, 9];
+console.log(sum(array1))
 
 
 //REVERSING AN ARRAY PARTS 1 & 2 P181
