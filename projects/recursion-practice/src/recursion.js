6ed33c01cda302b5d7ce5974a6eb6a591ec96711
Pete Dollas <pdollas@gmail.com>
2023-07-11 02:54:21 +0000

    Updated with Recursion Practice solutions to #1-6 of 35

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -6,17 +6,26 @@
 // factorial(5);  // 120
 var factorial = function(n) {
   //base
-  if (n<0){
+  if (n < 0){
     return null;
   }
-  else if (){}
-
+  else if (n === 0){
+    return 1;
+  }
+  
   //recursion
+  return n * factorial(n - 1)
 };
+
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  //an array; do I slice or just keep adding by index?
+  if (array.length === 0)
+  {return 0}
+
+  return array[0] + sum(array.slice(1))
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -26,18 +35,39 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n < 0) {n *= -1}
+  if (n === 0) {return true}
+  else if (n === 1) {return false}
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
-// sumBelow(10); // 45
+// sumBelow(10); // 45//982721 615 4321
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n < 0) {return (n + 1) + sumBelow(n + 1)}//-2 0 + -1
+  if (n === 1 || n === -1) {return 0}
+  if (n === 0) {return 0}
+  if (n > 0) {return (n - 1) + sumBelow(n - 1)}
 };
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
-var range = function(x, y) {
-};
+
+function range(x, y, answer=[]) {
+  if (x === y || x - y === 1 || y - x === 1) {return answer}
+  if (answer[answer.length - 1] === y - 1) {return answer}
+  else if (answer[answer.length - 1] === y + 1) {return answer}
+  if (x < y){
+  answer.push(x + 1);
+  range(x + 1, y, answer)
+  }
+  if (x > y){
+    answer.push(x - 1);
+    return range(x - 1, y, answer)
+  }
+  return answer;
+}
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
