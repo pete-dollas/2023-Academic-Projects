fb30957da74ff1ea38857f1b128750ad9202c0c6
Pete Dollas <pdollas@gmail.com>
2023-07-12 20:14:29 +0000

    Testing credentials on updated Gitpod Account; no content changes

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -237,7 +237,7 @@ var fibonacci = function(n) {
 
 // 25. Return the Fibonacci number located at index n of the Fibonacci sequence.
 // [0,1,1,2,3,5,8,13,21]
-// nthFibo(5); // 5
+// nthFibo(5); // 5//test
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
