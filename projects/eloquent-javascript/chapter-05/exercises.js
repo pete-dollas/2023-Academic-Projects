e02eb30d504bfedc2d79d2e769d50483cf29acb0
Pete Dollas <pdollas@gmail.com>
2023-07-18 21:15:13 +0000

    Updated with solutions to Eloquent Javascript Ch5 #3 of 4

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -16,22 +16,35 @@ return flat;
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+DIRECTIONS
 //“Write a higher-order function loop that provides something like a for loop statement.
 // It takes a value, a test function, an update function, and a body function. Each iteration, 
 //it first runs the test function on the current loop value and stops if that returns false. Then 
 //it calls the body function, giving it the current value. Finally, it calls the update function to 
 //create a new value and starts from the beginning.
 //When defining the function, you can use a regular loop to do the actual looping.”
+/*
+Write a higher-order function called loop that mimics a for loop statement for iterating over an array. 
+The loop function takes four arguments: value (the array to be iterated), test (a function that tests each element 
+  in the array), update (a function that updates the value for the next iteration), and 
+  body (a function that operates on the current element during each iteration).
 
+During each iteration, the loop function runs the test function on the current element of the array. 
+If the test function returns false, the loop stops. Otherwise, the body function is called, passing the 
+current element as an argument. After the body function completes, the update function is called to create a new 
+value for the next iteration.
 
-function loop(value, test, update, body) {
-  for (let i = 0; i < value.length; i++){
-  if (test(value[i]) !== false) {
-    let bod = body(value[i]);
-    update(bod);
-}
+Please note that the current solution provided is incomplete, and you need to implement the missing parts to 
+make the loop function fully functional. Additionally, ensure that the test, update, and body functions are 
+implemented correctly to avoid the TypeError issues observed in the test cases.
+*/
+function loop(start, test, update, body) {
+  for (let value = start; test(value); value = update(value)) {
+    body(value);
   }
 }
+
+
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
@@ -42,10 +55,12 @@ function loop(value, test, update, body) {
 //Write two versions, one using a loop and one using the some method.”
 
 
-function every() {
-
-}
-
+function every(array, func) {
+  for (let i = 0; i < array.length; i++){
+    if (func(array[i]) !== true) {return false}
+  }
+  return true;
+  }
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
@@ -57,7 +72,7 @@ function every() {
 //are probably useful here.”
 
 
-function dominantDirection() {
+function dominantDirection(text) {
 
 }
 
